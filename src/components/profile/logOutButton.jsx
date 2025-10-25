0bf71812def30b89bee1e3d694d653f7e5c88842
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-25 11:27:07 +0200

    new logout icon

--- src/components/profile/logOutButton.jsx
+++ src/components/profile/logOutButton.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../../auth/authContext.jsx";
+import { LogOut } from "lucide-react";
 
 export default function LogOutButton() {
     const { user, loading, logout } = useAuth();
@@ -13,21 +14,7 @@ export default function LogOutButton() {
                 onClick={logout}
                 className="flex items-center text-red-400 hover:text-red-500 cursor-pointer transition-colors duration-200"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10 19l-7-7m0 0l7-7m-7 7h18"
-                    />
-                </svg>
-                <span className="font-medium">Sign Out</span>
+                <LogOut className="h-5 w-5 mr-2" /><span className="font-medium">Sign Out</span>
             </a>
         </div>
     );
