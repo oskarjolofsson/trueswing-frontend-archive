af11997e7c17618c12d37126688d088fd4fe9e2b
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-31 19:17:55 +0100

    debug stripe backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,20 @@ import PricingTable from "../components/subscriptions/PricingTable.jsx";
 
 import MarketingBox from "../components/home-page/Marketingbox.jsx";
 
+import { getAuth } from "firebase/auth";
+
 export default function Home() {
+
+    const auth = getAuth();
+    auth.onAuthStateChanged(async (user) => {
+    if (user) {
+        const token = await user.getIdToken();
+        console.log("Firebase ID token:", token);
+    } else {
+        console.log("No user signed in");
+    }
+    });
+
     return (
         <div>
             <Hero />
