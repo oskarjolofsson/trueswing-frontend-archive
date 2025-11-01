b8a36f2d5e92bc7969570ccc78536fec8538e92b
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-01 16:25:07 +0100

    saving user info on login

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,16 +1,33 @@
 import { signInWithPopup, signOut } from "firebase/auth";
 import { auth, googleProvider} from "../lib/firebase.js";
+const URL = import.meta.env.VITE_API_URL;
 
 export async function login() {
     const result = await signInWithPopup(auth, googleProvider);
 
-    // TODO cookies thorugh backend
+    // Add user info to your database if needed
+    const user = result.user;
+    await registerUserInBackend(user);
 
     return result;
 }
 
 
 export async function logout() {
-    // TODO remove cookie from flask backend
     await signOut(auth);
+}
+
+async function registerUserInBackend(user) {
+    const token = await user.getIdToken();
+
+    await fetch(`${URL}/api/v1/users/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            uid: user.uid,
+        }),
+    });
 }
