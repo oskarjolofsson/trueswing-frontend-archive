bcc2f7556a21b7aafdd03fe8e0cdb21348fb8f95
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-01-14 11:48:00 +0100

    switched back to old firebase project

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,13 +9,13 @@ import { getAnalyticsConsent } from "./consent.js";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_TS,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_TS,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_TS,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORRAGE_BUCKET_TS, 
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_TS,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID_TS,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_TS,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORRAGE_BUCKET, 
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
