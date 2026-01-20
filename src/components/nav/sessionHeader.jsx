05bc68d586555ac5d160beb0296115d914ce5ec5
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-01-20 22:00:02 +0100

    sessionHeader border

--- src/components/nav/sessionHeader.jsx
+++ src/components/nav/sessionHeader.jsx
@@ -6,7 +6,7 @@ export default function sessionHeader() {
     // Session-header placed on top of the screen
     // Share-button on the right side
     return (
-        <div className="h-16  flex items-center justify-end px-6 w-full">
+        <div className="h-16 border-b border-l border-white/10 rounded-l-xl flex items-center justify-end px-6 w-full ml-2">
             
             <div className="flex items-center gap-4">
                 <div className="flex items-center gap-2">
