5797d7d9cee63c6869f3de823c5870559de93c7b
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-02-17 20:57:20 +0100

    allow for TypeScript in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 import { RouterProvider } from "react-router-dom";
-import { onAuthStateChanged, signOut, signInWithPopup } from "firebase/auth";
+import { onAuthStateChanged, signOut, signInWithPopup, User } from "firebase/auth";
 import { auth, googleProvider } from "./lib/firebase.js";
 import { router } from "./app/router.jsx";
 import './App.css'
 
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     return onAuthStateChanged(auth, setUser);
