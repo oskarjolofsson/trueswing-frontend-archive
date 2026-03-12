6085f6938097c70f55a440326c2cfacc71d10798
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-03-12 22:44:49 +0100

    services and routes for practice_session

---
+++ src/features/drills/services/practiceService.ts
@@ -0,0 +1,25 @@
+
+
+async function startPracticeSession(userId: string, drillId: string) {
+
+}
+
+
+async function endPracticeSession(userId: string, drillId: string) {
+
+}
+
+
+async function startDrillRun(sessionId: string, drillId: string) {
+
+}
+
+
+async function endDrillRun(drillRunId: string) {
+
+}
+
+
+async function recordRep(drillRunId: string, repData: any) {
+
+}
