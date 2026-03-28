8fb402802817a97dd45cc2066d131fbfdd269e51
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-03-28 11:35:29 +0100

    Make sure not logged in users still get redirected to shared-link after signed in

--- src/features/auth/routes/RequireAuth.tsx
+++ src/features/auth/routes/RequireAuth.tsx
@@ -1,20 +1,23 @@
 
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
-import SignInPopup from '@/shared/components/popup/signInPopup';
-import { useNavigate } from 'react-router-dom';
 
 export default function RequireAuth() {
   const { user, loading } = useAuth()
-  const navigate = useNavigate()
 
-  // Still checking session
   if (loading) {
     return <div>Loading...</div>
   }
 
   // Not logged in
   if (!user) {
+
+    // Store the intended path for post-login redirect
+    const intendedPath = location.pathname + location.search;
+    if (intendedPath !== "/") {
+      sessionStorage.setItem("postLoginRedirect", intendedPath);
+    }
+
     return <Navigate to="/?auth=signin" replace />;   // Redirect to landing page with sign-in popup
   }
 
--- src/features/auth/screens/AuthCallbackScreen.tsx
+++ src/features/auth/screens/AuthCallbackScreen.tsx
@@ -1,10 +1,12 @@
 import { supabase } from '@/lib/supabase';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {useRef } from 'react';
 
 export default function AuthCallbackPage() {
   const navigate = useNavigate()
   const [errorCode, setErrorCode] = useState<string | null>(null)
+  const hasHandledAuthRef = useRef(false);
 
   useEffect(() => {
     const handleCallback = async () => {
@@ -15,8 +17,20 @@ export default function AuthCallbackPage() {
         return
       }
 
+      if (!data.session || hasHandledAuthRef.current) return;
+
       if (data.session) {
-        navigate('/dashboard/app')
+        // Mark that we've handled the auth callback to prevent multiple redirects
+        hasHandledAuthRef.current = true;
+        // Use is loggeed in now, redirect to dashboard or intended path
+        const redirect = sessionStorage.getItem("postLoginRedirect");
+        console.log("Post-login redirect path:", redirect);
+        if (redirect && redirect.startsWith("/")) {
+          sessionStorage.removeItem("postLoginRedirect");
+          navigate(redirect, { replace: true });
+        } else {
+          navigate("/dashboard/app", { replace: true });
+        }
       } else {
         setErrorCode('no session created')
       }
